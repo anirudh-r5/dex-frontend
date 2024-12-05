9669dc22834854f54565e168b0d2e7979661281a
anirudh-r5 <anirudh.r515@gmail.com>
2024-12-05 03:43:05 -0800

    updated staking code
    Signed-off-by: anirudh-r5 <anirudh.r515@gmail.com>

--- src/app/components/ui/dashboard/staking/Stake.tsx
+++ src/app/components/ui/dashboard/staking/Stake.tsx
@@ -1,39 +1,113 @@
 'use client';
 
 import { stakerAbi } from '@/app/lib/generated';
-import { useReadContract, useWriteContract } from 'wagmi';
+import {
+  useAccount,
+  useReadContract,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from 'wagmi';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { formatEther, parseEther } from 'viem';
+import { AnimatePresence, motion } from 'motion/react';
 import { useQueryClient } from '@tanstack/react-query';
+import dayjs from 'dayjs';
 
 export default function Stake() {
   const queryClient = useQueryClient();
-  const stakeAddress = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
-  const { data: timeLeft, queryKey } = useReadContract({
+  const stakeAddress = '0x59b670e9fA9D0A427751Af201D676719a970857b';
+  const { address } = useAccount();
+  const [stake, setStake] = useState('');
+  const [target, setTarget] = useState<bigint>(BigInt(0));
+  const [raised, setRaised] = useState<bigint>(BigInt(0));
+  const [selfRaised, setSelfRaised] = useState<bigint>(BigInt(0));
+  const [deadline, setDeadline] = useState<number>();
+  const [countdownTime, setCountdownTime] = useState({ h: 0, m: 0, s: 0 });
+  const [notifyStake, setnotifyStake] = useState(true);
+  const { data: timeLeft } = useReadContract({
     abi: stakerAbi,
     address: stakeAddress,
-    functionName: 'timeLeft',
+    functionName: 'deadline',
   });
+
+  const { data: targetEth } = useReadContract({
+    abi: stakerAbi,
+    address: stakeAddress,
+    functionName: 'threshold',
+  });
+
+  const { data: raisedEth, queryKey } = useReadContract({
+    abi: stakerAbi,
+    address: stakeAddress,
+    functionName: 'raised',
+  });
+
+  const { data: selfEth } = useReadContract({
+    abi: stakerAbi,
+    address: stakeAddress,
+    functionName: 'balances',
+    args: [address],
+  });
+
   const {
     data: stakeData,
     isPending: stakePending,
     writeContractAsync: stakeWriter,
   } = useWriteContract();
 
+  const { isLoading: stakeConfirming, isSuccess: stakeConfirmed } =
+    useWaitForTransactionReceipt({
+      hash: stakeData,
+    });
+
   async function stakeEth() {
     await stakeWriter({
       abi: stakerAbi,
       address: stakeAddress,
       functionName: 'stake',
+      value: parseEther(stake),
     });
-    console.log(stakeData);
+    await queryClient.invalidateQueries({ queryKey });
+    setnotifyStake(true);
   }
 
-  async function checkTimeLeft() {
-    await queryClient.invalidateQueries({ queryKey });
-    console.log(timeLeft);
+  function handleStake(e: ChangeEvent<HTMLInputElement>) {
+    setStake(e.target.value);
   }
+
+  function dismissNotify() {
+    setnotifyStake(false);
+  }
+
+  useEffect(() => {
+    if (targetEth) setTarget(targetEth);
+    if (raisedEth) setRaised(raisedEth);
+    if (selfEth) setSelfRaised(selfEth);
+    setDeadline(Number(timeLeft));
+    const limit = dayjs(Number(timeLeft));
+    const dead = {
+      h: limit.diff(dayjs(), 'h'),
+      m: limit.diff(dayjs(), 'm'),
+      s: limit.diff(dayjs(), 's'),
+    };
+    setCountdownTime(dead);
+  }, [targetEth, raisedEth, selfEth, timeLeft]);
+
+  useEffect(() => {
+    const updater = setInterval(() => {
+      const limit = dayjs(deadline);
+      const dead = {
+        h: limit.diff(dayjs(), 'h'),
+        m: limit.diff(dayjs(), 'm'),
+        s: limit.diff(dayjs(), 's'),
+      };
+      setCountdownTime(dead);
+    }, 1000);
+    return () => clearInterval(updater);
+  }, [deadline]);
 
   return (
-    <div className="card bg-base-200 shadow-2xl my-4">
+    <div className="card bg-base-200 shadow-2xl my-4 w-500">
       <div className="card-body text-center items-center">
         <h2 className="card-title">Stake Contract</h2>
         <p className="italic">{`Address: ${stakeAddress}`}</p>
@@ -41,27 +115,25 @@ export default function Stake() {
           <div className="stats stats-vertical shadow-xl bg-base-300">
             <div className="stat">
               <div className="stat-title">Target ETH</div>
-              <div className="stat-value text-primary">10 ETH</div>
+              <div className="stat-value text-primary">{`${formatEther(target)} ETH`}</div>
             </div>
 
             <div className="stat">
               <div className="stat-title">Time Remaining</div>
               <div className="stat-value text-warning">
-                <span className="countdown font-mono text-2xl">
-                  <span></span>h<span></span>m<span></span>s
-                </span>
+                <span className="font-mono text-2xl">{`${countdownTime.h}h ${countdownTime.m}m ${countdownTime.s}s`}</span>
               </div>
             </div>
           </div>
           <div className="stats stats-vertical shadow-xl bg-base-300">
             <div className="stat">
               <div className="stat-title">Total Staked</div>
-              <div className="stat-value text-error"> 5 / 10 ETH</div>
+              <div className="stat-value text-error">{`${formatEther(raised)}/${formatEther(target)} ETH`}</div>
             </div>
 
             <div className="stat">
               <div className="stat-title">Your Stake</div>
-              <div className="stat-value text-success">2 ETH</div>
+              <div className="stat-value text-success">{`${formatEther(selfRaised)} ETH`}</div>
             </div>
           </div>
         </div>
@@ -78,6 +150,8 @@ export default function Stake() {
               placeholder="Enter amount"
               className="input input-bordered input-primary join-item"
               disabled={stakePending}
+              value={stake}
+              onChange={handleStake}
             />
             <button
               className="btn join-item btn-primary"
@@ -88,6 +162,99 @@ export default function Stake() {
             </button>
           </div>
         </div>
+        <AnimatePresence>
+          {stakePending && (
+            <motion.div
+              role="alert"
+              className="alert alert-warning"
+              initial={{ opacity: 0, scale: 0.85 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.85 }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                />
+              </svg>
+              <span>Processing transaction</span>
+            </motion.div>
+          )}
+          {stakeConfirming && (
+            <motion.div
+              role="alert"
+              className="alert alert-info"
+              initial={{ opacity: 0, scale: 0.85 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.85 }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="h-6 w-6 shrink-0 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <span>Awaiting confirmation</span>
+            </motion.div>
+          )}
+          {stakeConfirmed && notifyStake && (
+            <motion.div
+              role="alert"
+              className="alert alert-success"
+              initial={{ opacity: 0, scale: 0.85 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.85 }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>
+                <p>Transaction success!</p>
+                <p>Hash: {stakeData}</p>
+              </span>
+              <button className="btn btn-square btn-sm" onClick={dismissNotify}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
--- src/app/lib/generated.ts
+++ src/app/lib/generated.ts
@@ -1,4 +1,106 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// IERC20
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const ierc20Abi = [
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      {
+        name: 'owner',
+        internalType: 'address',
+        type: 'address',
+        indexed: true,
+      },
+      {
+        name: 'spender',
+        internalType: 'address',
+        type: 'address',
+        indexed: true,
+      },
+      {
+        name: 'value',
+        internalType: 'uint256',
+        type: 'uint256',
+        indexed: false,
+      },
+    ],
+    name: 'Approval',
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'from', internalType: 'address', type: 'address', indexed: true },
+      { name: 'to', internalType: 'address', type: 'address', indexed: true },
+      {
+        name: 'value',
+        internalType: 'uint256',
+        type: 'uint256',
+        indexed: false,
+      },
+    ],
+    name: 'Transfer',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'tokenOwner', internalType: 'address', type: 'address' },
+      { name: 'spender', internalType: 'address', type: 'address' },
+    ],
+    name: 'allowance',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'spender', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [{ name: 'account', internalType: 'address', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'to', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'from', internalType: 'address', type: 'address' },
+      { name: 'to', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'transferFrom',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }],
+    stateMutability: 'nonpayable',
+  },
+] as const
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // StakeExecContract
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -78,6 +180,13 @@ export const stakerAbi = [
   {
     type: 'function',
     inputs: [],
+    name: 'raised',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
     name: 'stake',
     outputs: [],
     stateMutability: 'payable',
@@ -111,5 +220,282 @@ export const stakerAbi = [
     name: 'withdraw',
     outputs: [],
     stateMutability: 'nonpayable',
+  },
+] as const
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Token
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const tokenAbi = [
+  {
+    type: 'constructor',
+    inputs: [
+      { name: '_name', internalType: 'string', type: 'string' },
+      { name: '_symbol', internalType: 'string', type: 'string' },
+    ],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      {
+        name: 'owner',
+        internalType: 'address',
+        type: 'address',
+        indexed: true,
+      },
+      {
+        name: 'spender',
+        internalType: 'address',
+        type: 'address',
+        indexed: true,
+      },
+      {
+        name: 'value',
+        internalType: 'uint256',
+        type: 'uint256',
+        indexed: false,
+      },
+    ],
+    name: 'Approval',
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      {
+        name: 'previousOwner',
+        internalType: 'address',
+        type: 'address',
+        indexed: true,
+      },
+      {
+        name: 'newOwner',
+        internalType: 'address',
+        type: 'address',
+        indexed: true,
+      },
+    ],
+    name: 'OwnershipTransferred',
+  },
+  {
+    type: 'event',
+    anonymous: false,
+    inputs: [
+      { name: 'from', internalType: 'address', type: 'address', indexed: true },
+      { name: 'to', internalType: 'address', type: 'address', indexed: true },
+      {
+        name: 'value',
+        internalType: 'uint256',
+        type: 'uint256',
+        indexed: false,
+      },
+    ],
+    name: 'Transfer',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'tokenOwner', internalType: 'address', type: 'address' },
+      { name: 'spender', internalType: 'address', type: 'address' },
+    ],
+    name: 'allowance',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'spender', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [{ name: 'account', internalType: 'address', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'account', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'burn',
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'decimals',
+    outputs: [{ name: '', internalType: 'uint8', type: 'uint8' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'account', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'name',
+    outputs: [{ name: '', internalType: 'string', type: 'string' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'owner',
+    outputs: [{ name: '', internalType: 'address', type: 'address' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'symbol',
+    outputs: [{ name: '', internalType: 'string', type: 'string' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'to', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: 'from', internalType: 'address', type: 'address' },
+      { name: 'to', internalType: 'address', type: 'address' },
+      { name: 'amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'transferFrom',
+    outputs: [{ name: '', internalType: 'bool', type: 'bool' }],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [{ name: 'newOwner', internalType: 'address', type: 'address' }],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+] as const
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// TokenSwap
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const tokenSwapAbi = [
+  {
+    type: 'constructor',
+    inputs: [
+      { name: '_token1', internalType: 'address', type: 'address' },
+      { name: '_token2', internalType: 'address', type: 'address' },
+    ],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: '_token1Amount', internalType: 'uint256', type: 'uint256' },
+      { name: '_token2Amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'addLiquidity',
+    outputs: [
+      { name: 'liquidityAdded', internalType: 'uint256', type: 'uint256' },
+    ],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'getBalances',
+    outputs: [
+      { name: 'token1Balance', internalType: 'uint256', type: 'uint256' },
+      { name: 'token2Balance', internalType: 'uint256', type: 'uint256' },
+    ],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'getTotalLiquidity',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [{ name: '', internalType: 'address', type: 'address' }],
+    name: 'liquidity',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [{ name: '_amount', internalType: 'uint256', type: 'uint256' }],
+    name: 'removeLiquidity',
+    outputs: [
+      { name: 'token1Amount', internalType: 'uint256', type: 'uint256' },
+      { name: 'token2Amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [
+      { name: '_fromToken', internalType: 'address', type: 'address' },
+      { name: '_toToken', internalType: 'address', type: 'address' },
+      { name: '_amount', internalType: 'uint256', type: 'uint256' },
+    ],
+    name: 'swap',
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'token1',
+    outputs: [{ name: '', internalType: 'address', type: 'address' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'token2',
+    outputs: [{ name: '', internalType: 'address', type: 'address' }],
+    stateMutability: 'view',
+  },
+  {
+    type: 'function',
+    inputs: [],
+    name: 'totalLiquidity',
+    outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+    stateMutability: 'view',
   },
 ] as const
